f61c105cf5db85bffc556e6f85cf772de2a06c78
Ann Jose <annjose0612@gmail.com>
2022-08-10 10:26:44 +0800

    formating form and validations

--- app/models/health_declaration_form.rb
+++ app/models/health_declaration_form.rb
@@ -1,4 +1,6 @@
 class HealthDeclarationForm < ApplicationRecord
     validates :name, presence: true, length: {minimum:4, maximum:50}
     validates :temperature, presence: true
+    validates :symptom, inclusion: { in: [ true, false ], message: "Please choose an option"  }
+    validates :suspect_contact, inclusion: { in: [ true, false ], message: "Please choose an option"  }
 end
